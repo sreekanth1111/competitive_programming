d76207be333560bacf0edefbd2a30c48ebe71761
sreekanth1111 <sreekanthr688@gmail.com>
2025-08-22 12:51:36 +0530

    add mainak array

---
+++ cp31/900/mainak_array.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+
+    while(t--){
+
+        int n; cin >> n;
+        vector<int> arr(n);
+
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+            
+        }
+
+        int val = 0;
+
+        for(int i=1; i<n; i++){
+            val = max(val,arr[i]-arr[0]);
+        }
+
+        for(int i=0; i<n-1; i++){
+            val = max(val,arr[n-1]-arr[i]);
+        }
+
+        for(int i=0; i<n-1; i++){
+            val = max(val,arr[i]-arr[i+1]);
+        }
+
+        cout << val << endl;
+        
+
+    }
+    
+}
