e055dd27665d8a94915a1af4c5fba48a069b3ec7
sreekanth1111 <sreekanthr688@gmail.com>
2025-10-06 21:04:47 +0530

    add perfectly balanced string

--- cp31/1100/perfectly_balanced_string.cpp
+++ cp31/1100/perfectly_balanced_string.cpp
@@ -7,7 +7,31 @@ int main(){
 
     while(t--){
         
-        
+        string s; cin >> s;
+        set<char> st;
+
+        for(int i=0; i<s.length(); i++)  st.insert(s[i]);
+
+        int k = st.size();
+        bool isok = true;
+
+        for(int i=0; i<s.length(); i++){
+
+            string str = s.substr(i,k);
+            vector<int> a(26,0);
+            for(char ch : str){
+                a[ch-'a']++;
+                if(a[ch-'a']>=2){
+                    isok = false;
+                    break;
+                }
+            }
+            if(!isok) break;
+        }
+
+        cout << (isok ? "YES" : "NO") << endl;
+
+
     }
 
     
